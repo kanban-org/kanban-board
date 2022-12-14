6d3fa5155a774e6ec941b5531c19d7d97563ca43
Gargi Chaurasia <gargi.0901@gmail.com>
2022-12-14 21:11:44 +0530

    Header section layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import classes from "./Header.module.scss";
+import icons from "../../img/symbol-defs.svg";
 
 function Header() {
-	return <div className={classes.header}>Header</div>;
+	return (
+		<div className={classes.header}>
+			<h2 className="heading--2">Platform launch</h2>
+			<button className="btn btn-primary">+Add new task</button>
+			<button className="btn">
+				<svg className="svg">
+					<use href={icons + "#icon-menu"}></use>
+				</svg>
+			</button>
+		</div>
+	);
 }
 
 export default Header;
