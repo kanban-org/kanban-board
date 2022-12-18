20620e184ca9f83c96c9b87a16531eb4531de9ec
Gargi Chaurasia <gargi.0901@gmail.com>
2022-12-18 14:29:44 +0530

    Scrollbar class added globally

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+
 import classes from "./Sidebar.module.scss";
 import logo_dark from "../../img/Dark-Mode-Logo.png";
 import logo_light from "../../img/Light-Mode-Logo.png";
+import icons from "../../img/symbol-defs.svg";
 
 function Sidebar(props) {
+	const lightMode = (
+		<button className={classes.toggleBtn} onClick={props.onClickMode}>
+			<span>Dark</span>
+			<svg className="svg svg-dark">
+				<use href={icons + "#icon-dark"}></use>
+			</svg>
+		</button>
+	);
+
+	const darkMode = (
+		<button className={classes.toggleBtn} onClick={props.onClickMode}>
+			<span>Light</span>
+			<svg className="svg svg-white">
+				<use href={icons + "#icon-light"}></use>
+			</svg>
+		</button>
+	);
+
 	return (
 		<div className={classes.sidebar}>
 			<div className={classes.logo}>
@@ -20,6 +40,8 @@ function Sidebar(props) {
 					kanban
 				</h1>
 			</div>
+
+			{props.theme === "dark" ? darkMode : lightMode}
 		</div>
 	);
 }
