411e1f3b233896f31b8ba3784a6d0c33d67dd170
Gargi Chaurasia <gargi.0901@gmail.com>
2022-12-21 21:36:08 +0530

    Added props

--- src/components/TaskDetails/TaskDetails.jsx
+++ src/components/TaskDetails/TaskDetails.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import classes from "./TaskDetails.module.scss";
 
-function Task() {
+function Task({ id, title, totalSubtasks, comepletedSubtasks }) {
 	return (
 		<>
-			<h3 className="heading--3">Build UI for onboarding flow</h3>
-			<h6 className="heading--6">0 of 4 subtasks</h6>
+			<h3 className="heading--3">{title}</h3>
+			<h6 className="heading--6">
+				{comepletedSubtasks} of {totalSubtasks} subtasks
+			</h6>
 		</>
 	);
 }
