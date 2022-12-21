7b588b8dbd3fe38005ad11171bcec44dd722d5fa
Gargi Chaurasia <gargi.0901@gmail.com>
2022-12-21 21:31:08 +0530

    Added close button to the modal component

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import classes from "./Modal.module.scss";
 
 function Modal(props) {
-	return <div className={classes.modal}>{props.children}</div>;
+	return (
+		<div className={classes.modal}>
+			{props.children}
+			<button className={classes.modalCloseBtn} onClick={props.onCloseModal}>
+				&#x2715;
+			</button>
+		</div>
+	);
 }
 
 export default Modal;
